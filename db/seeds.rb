7a78445e82e1d7cfe2ffc9a29e12fc1c44d8f56a
Alexa <alexa3000g@outlook.com>
2023-03-01 20:38:02 +0800

    feat: scaffold board/post
    rails generate scaffold board name:string
    rails generate scaffold post title:string content:text

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do |i|
+  Board.create(name: "board ##{i+1}")
+  2.times do |j|
+    Post.create(title: "title for b#{i+1} p#{j+1}", content: "content for board ##{i+1} post ##{j+1}")
+  end
+end
