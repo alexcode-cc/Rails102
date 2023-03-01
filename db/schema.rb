555cd70e7eef487ab7dbeca3266674b53857c8b1
Alexa <alexa3000g@outlook.com>
2023-03-01 20:45:09 +0800

    feat: add board id to post
    has_many :posts
    belongs_to :boards
    rails generate migration add_board_id_to_post board_id:integer
    resources :boards do
      resources :posts
    end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230301122648) do
+ActiveRecord::Schema.define(version: 20230301123920) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20230301122648) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "board_id"
   end
 
 end
