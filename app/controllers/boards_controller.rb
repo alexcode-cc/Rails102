dd0fcd4a378026b62fd35e677c11ac200d0b57a7
alex <alex@ubuntu>
2023-03-02 08:14:36 +0000

    feat: Install devise
    rails generate devise:install
    rails generate:views
    rails generate devise user

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,6 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: %i[ show edit update destroy ]
+  before_action :authenticate_user! , only: [:new]
 
   # GET /boards or /boards.json
   def index
